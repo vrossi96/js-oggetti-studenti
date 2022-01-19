434f5b4c65ec7aeb4384623a1c1e77493e2672e6
vrossi96 <vrossi396@yahoo.it>
2022-01-19 16:30:16 +0100

    add input from page

--- js/script.js
+++ js/script.js
@@ -9,26 +9,19 @@ Usiamo il DOM per stampare e chiedere le informazioni all'utente!
 */
 // Prendo il container
 const container = document.getElementById('container');
+const inputName = document.getElementById('name');
+const inputLastname = document.getElementById('lastname');
+const inputAge = document.getElementById('age');
+const inputButton = document.getElementById('refresh');
+
 
 
 //!Creare un oggetto con proprietà nome, cognome e età.
 // Ho un object con 3 proprietà
 const classe = [
-   {
-      nome: 'Mario',
-      cognome: 'Bros', 
-      eta: 20,
-   },
-   {
-      nome: 'Luigi',
-      cognome: 'Bros', 
-      eta: 20,
-   },
-   {
-      nome: 'Pinco', 
-      cognome: 'Pallo', 
-      eta: 75,
-   },
+   { nome: 'Mario', cognome: 'Bros', eta: 20, },
+   { nome: 'Luigi', cognome: 'Bros', eta: 20, },
+   { nome: 'Pinco', cognome: 'Pallo', eta: 75, },
 ];
 
 //Stampa in console tutto l'oggetto
@@ -36,10 +29,6 @@ console.table(classe);
 console.log(classe[0]);
 console.log(classe[1]);
 
-// Chiedo i dati per nome cognome ed età
-const newName = prompt('Inserisci il nome');
-const newLastname = prompt('Inserisci il cognome');
-const newAge = prompt("Inserisci l'età");
 
 // Creo nuovo oggetto da pushare in seguito
 const newUser = {
@@ -48,31 +37,40 @@ const newUser = {
    eta: 'età',
 };
 
-// Riassegno i valori dell'oggetto
-newUser.nome = newName;
-newUser.cognome = newLastname;
-newUser.eta = newAge;
-console.log(newUser);
-
-// Inserisco l'oggetto nell'array
-classe.push(newUser);
-
-
-
-//!Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
-// Per ogni array nell array classe
-for (let i = 0; i < classe.length; i++){
-   //* console.table(classe[i]);
-   // Prendo ogni valore nell'array 
-   for (let key in classe[i]) {
-      if (!(key === 'eta')) {
-         // Ad ogni ciclo viene creato un p
-         let info = document.createElement('p');
-         // Stampa il valore della key nell array (i)
-         let valore = classe[i][key];
-         
-         info.innerText = key + ': ' + valore;
-         container.appendChild(info);
+// Chiedo i dati per nome cognome ed età
+inputButton.addEventListener('click', function() {
+   const newName = inputName.value;
+   const newLastname = inputLastname.value;
+   const newAge = inputAge.value;
+   newUser.nome = newName;
+   newUser.cognome = newLastname;
+   newUser.eta = newAge;
+   // Riassegno i valori dell'oggetto
+   console.log(newUser);
+   
+   // Inserisco l'oggetto nell'array
+   classe.push(newUser);
+   console.table(classe);
+   //!Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+   // Per ogni array nell array classe
+   for (let i = 0; i < classe.length; i++){
+      //* console.table(classe[i]);
+      // Prendo ogni valore nell'array 
+      for (let key in classe[i]) {
+         if (!(key === 'eta')) {
+            // Ad ogni ciclo viene creato un p
+            let info = document.createElement('p');
+            // Stampa il valore della key nell array (i)
+            let valore = classe[i][key];
+            
+            info.innerText = key + ': ' + valore;
+            container.appendChild(info);
+         }
       }
    }
-}
+})
+
+
+
+
+
