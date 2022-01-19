684935112fd76041ae6fe0201fda020df5ab5344
vrossi96 <vrossi396@yahoo.it>
2022-01-19 15:26:23 +0100

    add in page results of all value

--- js/script.js
+++ js/script.js
@@ -9,33 +9,69 @@ Usiamo il DOM per stampare e chiedere le informazioni all'utente!
 */
 // Prendo il container
 const container = document.getElementById('container');
+// Creo un elemento vuoto
+
 
 //!Creare un oggetto con proprietà nome, cognome e età.
 // Ho un object con 3 proprietà
-const student = {
-   nome: 'Mario',
-   cognome: 'Bros',
-   eta: 20,
-}
-//Stampa in console tutto l'oggetto
-console.log(student);
+const classe = {
+   student: [
+      {nome: 'Mario', cognome: 'Bros', eta: 20,},
+      {nome: 'Luigi', cognome: 'Bros', eta: 20,},
+      {nome: 'Pinco', cognome: 'Pallo', eta: 75,},
+   ]
 
-//Stampa in console i singoli dati dell'oggetto
-console.log('Nome: ', student.nome);
-console.log('Cognome: ', student.cognome);
-console.log('Età: ', student.eta);
+};
+
+//Stampa in console tutto l'oggetto
+const studentsArray = classe.student;
+console.log(studentsArray);
+console.log(studentsArray[0]);
+console.log(studentsArray[1]);
+
+
+
+// Per ogni array...
+for (let i = 0; i < studentsArray.length; i++){
+   // Per ogni key in ogni array...
+   for (let key in studentsArray[i]){
+      // Ad ogni ciclo viene creato un p
+      let info = document.createElement('p');
+      let valore = studentsArray[i][key];
+      // Stampa il valore della key nell array (i)
+      console.log(valore);
+
+      info.innerText = key + ': ' + valore;
+      container.appendChild(info);
+   }
+}
 
 //!Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
-// Per ogni proprietà in student
-for (let key in student) {
+/*
+for (let key in classe){
+   for (let i = 0; i < classe[key].length; i++) {
+      console.log('ciao');
+      let info = classe[key][i];
+      console.log(info);
+   }  
+};
+*/
+
+
+
+
+
+
+// Per ogni proprietà in classe
+/*
+for (let key in classe) {
    // Creo un elemento p
    let info = document.createElement('p');
-   // Inserisco in p il valore della proprietà di student
-   info.innerText = key + ': ' + student[key];
+   // Inserisco in p il valore della proprietà di classe
+   info.innerText = key + ': ' + classe[key];
    console.log(`--Stampa ${key} con il for--`)
-   console.log(student[key]);
+   console.log(classe[key]);
    // Inserisco nel container il p completo
    container.appendChild(info);
 }
-
-
+*/
